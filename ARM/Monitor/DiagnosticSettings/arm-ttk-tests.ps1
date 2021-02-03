314738a3e1773162d90d4951c600950d1117a939
Keith Drew <keith.drew@objectsinternational.co.uk>
2021-02-03 10:59:38 +0000

    Change skip list to array

--- ARM/Monitor/DiagnosticSettings/arm-ttk-tests.ps1
+++ ARM/Monitor/DiagnosticSettings/arm-ttk-tests.ps1
@@ -1,1 +1,1 @@
-﻿Test-AzTemplate . -Skip "Location Should Not Be Hardcoded,Parameters Property Must Exist,Template Should Not Contain Blanks" -Pester
+﻿Test-AzTemplate . -Skip "Location Should Not Be Hardcoded","Parameters Property Must Exist","Template Should Not Contain Blanks" -Pester
