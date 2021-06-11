1533719c510f4a4c3c803e73856e2c3a14c822df
Keith Drew <keith.drew@objectsinternational.co.uk>
2021-06-11 08:38:45 +0100

    Add back the skipped tests, to be resolved later.

--- ARM/Monitor/DiagnosticSettings/arm-ttk-tests.ps1
+++ ARM/Monitor/DiagnosticSettings/arm-ttk-tests.ps1
@@ -1,1 +1,1 @@
-﻿Test-AzTemplate $PSScriptRoot -Skip "Secure Params In Nested Deployments" -Pester
+﻿Test-AzTemplate $PSScriptRoot -Skip "Secure Params In Nested Deployments", "Parameters Property Must Exist", "Template Should Not Contain Blanks" -Pester
