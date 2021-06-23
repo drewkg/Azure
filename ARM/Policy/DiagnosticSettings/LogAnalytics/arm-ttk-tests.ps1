243ad715c2f35d4259a55f60a2679fe60b7fbac6
Keith Drew <keith.drew@objectsinternational.co.uk>
2021-06-23 19:40:51 +0100

    Remove Secure Param skipped test

--- ARM/Policy/DiagnosticSettings/LogAnalytics/arm-ttk-tests.ps1
+++ ARM/Policy/DiagnosticSettings/LogAnalytics/arm-ttk-tests.ps1
@@ -1,1 +1,1 @@
-﻿Test-AzTemplate $PSScriptRoot -Skip "Secure Params In Nested Deployments", "Parameters Property Must Exist", "Template Should Not Contain Blanks" -Pester
+﻿Test-AzTemplate $PSScriptRoot -Skip "Parameters Property Must Exist", "Template Should Not Contain Blanks" -Pester
