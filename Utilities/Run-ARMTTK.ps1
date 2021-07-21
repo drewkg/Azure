6e4880fb27bd4f43169837944777b36f90babc71
Keith Drew <keith.drew@objectsinternational.co.uk>
2021-07-21 06:50:50 +0100

    Remove alias

--- Utilities/Run-ARMTTK.ps1
+++ Utilities/Run-ARMTTK.ps1
@@ -5,6 +5,6 @@ Expand-Archive -Path ".\ARM-TTK\ARM-TTK.zip" -DestinationPath ".\ARM-TTK" -Force
 Import-Module ".\ARM-TTK\arm-ttk\arm-ttk.psd1"
 Import-Module -Name Pester -MaximumVersion 4.10.1
 Set-Location "."
-Get-ChildItem -Name arm-ttk-tests.ps1 -recurse -Path . | ForEach {
+Get-ChildItem -Name arm-ttk-tests.ps1 -recurse -Path . | ForEach-Object {
   Invoke-Pester -Script (".\" + $_) -OutputFile (".\ARM-TTK\" + $_.Split('\')[$_.Split('\').Count - 2]  + ".nunit") -OutputFormat NUnitXml
 }
