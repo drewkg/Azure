3f68392452e9f1e8a24b4dd7217f7ef01c0eac4c
Keith Drew <keith.drew@objectsinternational.co.uk>
2021-07-28 16:18:24 +0100

    Removed skipped test as its going to be rolled back.

--- ARM/Resource/AppServiceWithMSIAppInsights/arm-ttk-tests.ps1
+++ ARM/Resource/AppServiceWithMSIAppInsights/arm-ttk-tests.ps1
@@ -1,3 +1,1 @@
-﻿Test-AzTemplate $PSScriptRoot -Pester -Skip "Resources Should Not Be Ambiguous"
-
-# Resources Should Not Be Ambiguous - Due to a bug with nested templates and inner scope.
+﻿Test-AzTemplate $PSScriptRoot -Pester
