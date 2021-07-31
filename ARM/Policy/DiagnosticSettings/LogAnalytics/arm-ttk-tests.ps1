d3eccedce61bbdc85222bd88b5d332fceb655081
Keith Drew <keith.drew@objectsinternational.co.uk>
2021-07-31 05:54:07 +0100

    Diable ARM TTK tests due to current bugs

--- ARM/Policy/DiagnosticSettings/LogAnalytics/arm-ttk-tests.ps1
+++ ARM/Policy/DiagnosticSettings/LogAnalytics/arm-ttk-tests.ps1
@@ -1,1 +1,4 @@
-﻿Test-AzTemplate $PSScriptRoot -Pester
+﻿# Test-AzTemplate $PSScriptRoot -Pester -Skip "apiVersions Should Be Recent"
+
+# Disable ARM TTK due to a JSON parsing issue with nested templates.
+# Disable apiVersions Should Be Recent - Due to a bug in ARM TTK for Diagnostic Settings
