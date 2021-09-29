c61ab937918fd652b4bb963897305a8040f2f9bb
Keith <drewkg@hotmail.com>
2021-09-29 14:08:47 +0100

    Log Analytics & Automation Account Update (#26)
    It was noticed that certain resource settings were missing from the template, this has now been corrected and the WhatIf report is much cleaner.
    
    Note when re-deploying the template, the properties.CreationDate and Workbook Properties do not exist as they are not in the general schema.

--- ARM/Resource/LogAnalytics&AutomationAccount/arm-ttk-tests.ps1
+++ ARM/Resource/LogAnalytics&AutomationAccount/arm-ttk-tests.ps1
@@ -1,3 +1,4 @@
-﻿Test-AzTemplate $PSScriptRoot -Pester -Skip "Location Should Not Be Hardcoded"
+﻿Test-AzTemplate $PSScriptRoot -Pester -Skip "Location Should Not Be Hardcoded", "DeploymentTemplate Must Not Contain Hardcoded Uri"
 
 # Location Should Not Be Hardcoded, as only allowed values so this test does not make sense.
+# DeploymentTemplate Must Not Contain Hardcoded Uri, this does not allow for URI's in params, known issue.
