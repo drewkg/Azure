b1777db9d9ac4fde5752f7bd981faf854e8a2965
Keith <drewkg@hotmail.com>
2021-09-29 20:54:56 +0100

    Introducing WhatIf Compare (#27)
    Introducing the ability to compare two WhatIf reports, one from ARM and one from Bicep. This should give us the ability to tell if the functionality is comparable.

--- Utilities/Invoke-WhatIfCompare.ps1
+++ Utilities/Invoke-WhatIfCompare.ps1
@@ -7,24 +7,85 @@ param (
   $Subscription,
   [switch]
   [parameter(ParameterSetName="RGSet")]
-  $ResourceGroup
+  $ResourceGroup,
+  [string]
+  [Parameter(ParameterSetName="MGSet")]
+  $ManagemengGroupId = "f0c0d850-482c-559b-82b2-b005c0aa2e7a",
+  [string]
+  [Parameter(ParameterSetName="SubSet")]
+  $SubscriptionName = "f0c0d850-482c-559b-82b2-b005c0aa2e7a",
+  [string]
+  $ARMTemplate,
+  [string]
+  $BicepFile
 )
 
+switch ( $true )
+{
+  $ManagementGroup
+  {
+    $bicepWhatIfResult = Get-AzManagementGroupDeploymentWhatIfResult -ManagementGroupId $ManagemengGroupId -Location UKSouth -TemplateFile .\Bicep\Policy\DiagnosticSettings\LogAnalytics\main.bicep
+    $armWhatIfResult = Get-AzManagementGroupDeploymentWhatIfResult -ManagementGroupId $ManagemengGroupId -Location UKSouth -TemplateFile .\ARM\Policy\DiagnosticSettings\LogAnalytics\azureDeploy.json -TemplateParameterFile .\ARM\Policy\DiagnosticSettings\LogAnalytics\azuredeploy.parameters.json
+  }
+  $Subscription
+  {
+    $bicepWhatIfResult = Get-AzSubscriptionDeploymentWhatIfResult -Name $SubscriptionName -Location UKSouth -TemplateFile .\Bicep\Policy\DiagnosticSettings\LogAnalytics\main.bicep
+    $armWhatIfResult = Get-AzSubscriptionDeploymentWhatIfResult -Name $SubscriptionName -Location UKSouth -TemplateFile .\ARM\Policy\DiagnosticSettings\LogAnalytics\azureDeploy.json -TemplateParameterFile .\ARM\Policy\DiagnosticSettings\LogAnalytics\azuredeploy.parameters.json
+  }
+  $ResourceGroup
+  {
+    $bicepWhatIfResult = Get-AzResourceGroupDeploymentWhatIfResult -ManagementGroupId $ManagemengGroupId -Location UKSouth -TemplateFile .\Bicep\Policy\DiagnosticSettings\LogAnalytics\main.bicep
+    $armWhatIfResult = Get-AzResourceGroupDeploymentWhatIfResult -ManagementGroupId $ManagemengGroupId -Location UKSouth -TemplateFile .\ARM\Policy\DiagnosticSettings\LogAnalytics\azureDeploy.json -TemplateParameterFile .\ARM\Policy\DiagnosticSettings\LogAnalytics\azuredeploy.parameters.json
+  }
+}
 
-$bicepWhatIfResult = Get-AzManagementGroupDeploymentWhatIfResult -ManagementGroupId f0c0d850-482c-559b-82b2-b005c0aa2e7a -Location UKSouth -TemplateFile .\Bicep\Policy\DiagnosticSettings\LogAnalytics\main.bicep
-$armWhatIfResult = Get-AzManagementGroupDeploymentWhatIfResult -ManagementGroupId f0c0d850-482c-559b-82b2-b005c0aa2e7a -Location UKSouth -TemplateFile .\ARM\Policy\DiagnosticSettings\LogAnalytics\azureDeploy.json -TemplateParameterFile .\ARM\Policy\DiagnosticSettings\LogAnalytics\azuredeploy.parameters.json
+$armNoChanges = $armWhatIfResult.Changes | Where-Object {$_.ChangeType -eq 'NoChange'}
+$bicepNoChanges = $bicepWhatIfResult.Changes | Where-Object {$_.ChangeType -eq 'NoChange'}
 
+If ($armNoChanges.Count -eq $bicepNoChanges.Count) {
+  Write-Host "Number of items with no change is the same"
+} Else {
+  $armNoChanges | ForEach-Object {
+    $itemtofind = $_.RelativeResourceId
 
-#Write-Host $bicepWhatIfResult.Changes.Count
+    $found = $bicepNoChanges | Where-Object {$_.RelativeResourceId -eq $itemtofind} | Measure-Object
 
+    if ($found.Count -eq 0) {
+      Write-Host $itemtofind "is unchanged in the ARM template, but not in the Bicep file."
+    }
+  }
+}
 
-#$bicepWhatIfResult.Changes | Select-Object -Unique | Where-Object { $armWhatIfResult.Changes -notcontains $_ }
-$diff = Compare-Object -ReferenceObject $armWhatIfResult.Changes -DifferenceObject $bicepWhatIfResult.Changes
+$armCreate = $armWhatIfResult.Changes | Where-Object {$_.ChangeType -eq 'Create'}
+$bicepCreate = $bicepWhatIfResult.Changes | Where-Object {$_.ChangeType -eq 'Create'}
 
-$diff
-#$armWhatIfResult.Changes | Select-Object -Unique | Where-Object { $bicepWhatIfResult.Changes -notcontains $_ }
+If ($armCreate.Count -eq $bicepCreate.Count) {
+  Write-Host "Number of items modified is the same"
+} Else {
+  $armCreate | ForEach-Object {
+    $itemtofind = $_.RelativeResourceId
 
-#$bicepWhatIfResult.Changes | ForEach-Object { Write-Host $_.FullyQualifiedResourceId }
-#Write-Host $armWhatIfResult.Changes.Count
+    $found = $bicepCreate | Where-Object {$_.RelativeResourceId -eq $itemtofind} | Measure-Object
 
-#$armWhatIfResult.Changes | ForEach-Object { Write-Host $_.FullyQualifiedResourceId }
+    if ($found.Count -eq 0) {
+      Write-Host $itemtofind "is created in the ARM template, but not in the Bicep file."
+    }
+  }
+}
+
+$armModifications = $armWhatIfResult.Changes | Where-Object {$_.ChangeType -eq 'Modify'}
+$bicepModification = $bicepWhatIfResult.Changes | Where-Object {$_.ChangeType -eq 'Modify'}
+
+If ($armModifications.Count -eq $bicepModification.Count) {
+  Write-Host "Number of items modified is the same"
+} Else {
+  $armModifications | ForEach-Object {
+    $itemtofind = $_.RelativeResourceId
+
+    $found = $bicepModification | Where-Object {$_.RelativeResourceId -eq $itemtofind} | Measure-Object
+
+    if ($found.Count -eq 0) {
+      Write-Host $itemtofind "is changed in the ARM template, but not in the Bicep file."
+    }
+  }
+}
