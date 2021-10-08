16a0c9d9629ff7210bddbe417d454f12d3df4f4e
Keith Drew <keith.drew@objectsinternational.co.uk>
2021-10-08 20:30:08 +0100

    Switch to using a Managed Identity

--- Runbook/Powershell/Update-AutomationAzureModulesForAccount.ps1
+++ Runbook/Powershell/Update-AutomationAzureModulesForAccount.ps1
@@ -502,7 +502,16 @@ if ($AzureModuleClass -eq "Az") {
 Update-ProfileAndAutomationVersionToLatest $AutomationModuleName
 
 if ($Login) {
-    Login-AzureAutomation $UseAzModule
+    #Login-AzureAutomation $UseAzModule
+
+    # Ensures you do not inherit an AzContext in your runbook
+    Disable-AzContextAutosave -Scope Process
+
+    # Connect to Azure with system-assigned managed identity
+    $AzureContext = (Connect-AzAccount -Identity).context
+
+    # set and store context
+    $AzureContext = Set-AzContext -SubscriptionName $AzureContext.Subscription -DefaultProfile $AzureContext
 }
 
 $ModuleImportMapOrder = Create-ModuleImportMapOrder $UseAzModule
