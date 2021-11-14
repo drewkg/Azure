e983e31e1bddaad2919e45858e7fb2b922aad824
Keith Drew <keith.drew@objectsinternational.co.uk>
2021-11-14 18:31:04 +0000

    Outline of Resource Graph query

--- Runbook/Powershell/Update-AzurePolicyComplianceState.ps1
+++ Runbook/Powershell/Update-AzurePolicyComplianceState.ps1
@@ -28,10 +28,10 @@
 
 [Diagnostics.CodeAnalysis.SuppressMessageAttribute("PSUseApprovedVerbs", "")]
 param(
-  [Parameter(Mandatory = $true)]
+  #[Parameter(Mandatory = $true)]
   [string] $ResourceGroupName,
 
-  [Parameter(Mandatory = $true)]
+  #[Parameter(Mandatory = $true)]
   [string] $AutomationAccountName,
 
   [string] $AzureEnvironment = 'AzureCloud',
@@ -40,3 +40,89 @@ param(
 )
 
 $ErrorActionPreference = "Continue"
+
+function Login-AzureAutomation([bool] $AzModuleOnly) {
+  try {
+    $RunAsConnection = Get-AutomationConnection -Name "AzureRunAsConnection"
+
+    if (!$RunAsConnection) {
+      Write-Output "Invalid or missing RunAs connection, attempting access using a MSI."
+      if ($AzModuleOnly) {
+        # Ensures you do not inherit an AzContext in your runbook
+        Disable-AzContextAutosave -Scope Process
+
+        # Connect to Azure with system-assigned managed identity
+        $AzureContext = (Connect-AzAccount -Identity).context
+
+        # set and store context
+        $AzureContext = Set-AzContext -SubscriptionName $AzureContext.Subscription -DefaultProfile $AzureContext
+        Select-AzSubscription -SubscriptionId $AzureContext.Subscription  | Write-Verbose
+      } else {
+        $AzureContext = (Connect-AzureRMAccount -Identity).context
+        Select-AzureRmSubscription -SubscriptionId $AzureContext.Subscription | Write-Verbose
+      }
+    } else {
+      Write-Output "Attempting access using RunAs account."
+      Write-Output "Logging in to Azure ($AzureEnvironment)."
+
+      if (!$RunAsConnection.ApplicationId) {
+        $ErrorMessage = "Connection 'AzureRunAsConnection' is incompatible type."
+        throw $ErrorMessage
+      }
+
+      if ($AzModuleOnly) {
+        Connect-AzAccount `
+            -ServicePrincipal `
+            -TenantId $RunAsConnection.TenantId `
+            -ApplicationId $RunAsConnection.ApplicationId `
+            -CertificateThumbprint $RunAsConnection.CertificateThumbprint `
+            -Environment $AzureEnvironment
+
+        Select-AzSubscription -SubscriptionId $RunAsConnection.SubscriptionID  | Write-Verbose
+
+        # Ensures you do not inherit an AzContext in your runbook
+        Disable-AzContextAutosave -Scope Process
+      } else {
+        Add-AzureRmAccount `
+            -ServicePrincipal `
+            -TenantId $RunAsConnection.TenantId `
+            -ApplicationId $RunAsConnection.ApplicationId `
+            -CertificateThumbprint $RunAsConnection.CertificateThumbprint `
+            -Environment $AzureEnvironment
+
+        Select-AzureRmSubscription -SubscriptionId $RunAsConnection.SubscriptionID  | Write-Verbose
+      }
+    }
+
+  } catch {
+    #if (!$RunAsConnection) {
+    #  $RunAsConnection | fl | Write-Output
+      Write-Output $_.Exception
+    #  $ErrorMessage = "Connection 'AzureRunAsConnection' not found."
+    #  throw $ErrorMessage
+    #}
+
+    throw $_.Exception
+  }
+}
+
+if ($Login) {
+  Login-AzureAutomation $UseAzModule
+}
+
+Write-Output "Querying Azure Resource Graph for Policy State"
+
+$KustoQuery = "
+policyresources
+| extend AssignmentScope = parse_json(properties).policyAssignmentScope
+| extend AssignmentName = tostring(parse_json(properties).policyAssignmentName)
+| where ['kind'] =='policystates'
+| summarize TotalResource = count(),
+    NotCompliant = countif(parse_json(properties).complianceState != 'Compliant')
+        by tostring(AssignmentName),
+        tostring(AssignmentScope),
+        subscriptionId
+ | order by tostring(AssignmentName) asc
+"
+$result = Search-AzGraph -Query $KustoQuery -ManagementGroup 4b1b011c-6812-45a0-8112-f41550d0f0c9 | Formet-Table
+Write-Output $result
