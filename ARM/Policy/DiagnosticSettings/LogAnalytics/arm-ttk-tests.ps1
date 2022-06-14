c5e54546108bd50a629a8bb6b0fa5610bfb90082
Keith Drew <keith.drew@objectsinternational.co.uk>
2022-06-14 22:17:14 +0100

    Enable disabled tests

--- ARM/Policy/DiagnosticSettings/LogAnalytics/arm-ttk-tests.ps1
+++ ARM/Policy/DiagnosticSettings/LogAnalytics/arm-ttk-tests.ps1
@@ -1,4 +1,1 @@
-﻿Test-AzTemplate $PSScriptRoot -Pester -Skip "apiVersions Should Be Recent"
-
-# Disable ARM TTK due to a JSON parsing issue with nested templates.
-# Disable apiVersions Should Be Recent - Due to a bug in ARM TTK for Diagnostic Settings
+﻿Test-AzTemplate $PSScriptRoot -Pester
