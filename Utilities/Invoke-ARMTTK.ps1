0bb7c9baf01acb029b5e3a57d6fab9ef9cb589df
Keith Drew <keith.drew@objectsinternational.co.uk>
2022-06-15 04:58:05 +0100

    Add ability to test subset of templates

--- Utilities/Invoke-ARMTTK.ps1
+++ Utilities/Invoke-ARMTTK.ps1
@@ -1,4 +1,5 @@
 param (
+  [string] $rootPath = ".\ARM\",
   [string] $outputPath = ".\ARM-TTK"
 )
 
@@ -19,7 +20,7 @@ Import-Module -Name Pester -MaximumVersion 4.10.1
 
 $failedTests = 0
 
-Get-ChildItem -Path .\ARM\* -Include arm-ttk-tests.ps1 -Recurse -File | ForEach-Object {
+Get-ChildItem -Path (Join-Path -Path $rootPath -ChildPath "*") -Include arm-ttk-tests.ps1 -Recurse -File | ForEach-Object {
   Write-Host "Running ARM-TTK on Directory - " $_.Directory.Name
   $TestResults = Invoke-Pester -PassThru -Script $_ -OutputFile (Join-Path -Path $outputPath -ChildPath ($_.Directory.Name + ".nunit")) -OutputFormat NUnitXml
   $failedTests += $TestResults.FailedCount
