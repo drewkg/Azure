092c58d117b1571d953c0feab39eaca865b448c3
Keith Drew <keith.drew@objectsinternational.co.uk>
2022-10-20 07:02:37 +0100

    disable recent api test as issue in ARM-TTK

--- ARM/Resource/LogAnalytics&AutomationAccount/arm-ttk-tests.ps1
+++ ARM/Resource/LogAnalytics&AutomationAccount/arm-ttk-tests.ps1
@@ -1,3 +1,4 @@
-﻿Test-AzTemplate $PSScriptRoot -Pester -Skip "Location Should Not Be Hardcoded"
+﻿Test-AzTemplate $PSScriptRoot -Pester -Skip "apiVersions Should Be Recent", "Location Should Not Be Hardcoded"
 
+# apiVersions Should Be Recent, as issue in ARM-TTK for linked services.
 # Location Should Not Be Hardcoded, as only allowed values so this test does not make sense.
